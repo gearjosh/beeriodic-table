65d68fd0341da105c33e21909f01c3f99fb12019
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-11-09 14:37:16 -0800

    add title component

---
+++ src/components/TitleBeeriodic.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function TitleBeeriodic(){
+  return (
+    <div  className="title">
+      <style jsx>{`
+      .title {
+        grid-column-start: 2;
+        grid-column-end: 7;
+        grid-row-start: 1;
+        grid-row-end: 2;
+        font-size: 6rem;
+        font-weight: bold;
+        padding: .125rem;
+      }
+    `}</style>
+      BEERIODIC
+    </div>
+  );
+}
+
+export default TitleBeeriodic;
