ec52c4ca6dc0a2ff202a902d9246d0125ae0077a
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-11-09 14:37:46 -0800

    add subtitle component

---
+++ src/components/TitleTable.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function TitleTable(){
+  return (
+    <div className="subtitle">
+      <style jsx>{`
+      .subtitle {
+        grid-column-start: 3;
+        grid-column-end: 6;
+        grid-row-start: 2;
+        grid-row-end: 3;
+        font-size: 6rem;
+        font-weight: bold;
+        padding: .125rem;
+      }
+    `}</style>
+      TABLE
+    </div>
+  );
+}
+
+export default TitleTable;
