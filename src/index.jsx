49782b4ad0a885c0ea5b39483034a5c8959d89a6
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-11-09 08:27:38 -0800

    add index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { AppContainer } from 'react-hot-loader';
+import { HashRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+
+document.body.style.margin = 0;
+
+const render = (Component) => {
+  const containerStyles = {
+    margin: 'none'
+  };
+  ReactDOM.render(
+    <AppContainer style={containerStyles}>
+      <HashRouter>
+        <Component/>
+      </HashRouter>
+    </AppContainer>,
+    document.getElementById('react-app-root')
+  );
+};
+
+render(App);
+
+/*eslint-disable */
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App);
+  });
+}
+/*eslint-enable */
